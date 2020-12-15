14c633f4db3377a430db0025c2c0cd6ef986442c
zakiyhisam <izzakiysamm@gmail.com>
2020-12-15 11:57:20 +0800

    Updates

---
+++ precache-manifest.209923484306f70bf0353936fa5f5568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a231a0fa2da329a8589417d5527bb7d",
+    "url": "/react-insta-clone/index.html"
+  },
+  {
+    "revision": "16588e23b0b5f2471c92",
+    "url": "/react-insta-clone/static/css/main.c13a2419.chunk.css"
+  },
+  {
+    "revision": "c8d45cd60866f42df143",
+    "url": "/react-insta-clone/static/js/2.2d976026.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/react-insta-clone/static/js/2.2d976026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16588e23b0b5f2471c92",
+    "url": "/react-insta-clone/static/js/main.a174bc76.chunk.js"
+  },
+  {
+    "revision": "8706c4e2754fcf4a6082",
+    "url": "/react-insta-clone/static/js/runtime-main.1b8c592b.js"
+  }
+]);
